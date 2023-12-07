fba1faad07a5c1043b3148d46e5a2babfbaedee1
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-12-07 14:58:08 -0600

    added an input and talking and binding between components

--- Heros2/src/app/app.module.ts
+++ Heros2/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 
@@ -12,7 +13,8 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
